66bc4e7cfa63cdc6e604d0890438cda7bb20f5ee
MartBent <martbent99@gmail.com>
2022-12-16 15:45:10 +0100

    Rewrote code to support deep sleeping, broke networking :c

--- main/main.c
+++ main/main.c
@@ -13,32 +13,44 @@ void setup_flash() {
     ESP_ERROR_CHECK( ret );
 }
 
-void gather_data_task(void* arg)
+void handle_timer_wakeup(settings_t* settings)
 {
     char* response = malloc(1000);
-    setup_sta((settings_t*)arg);
+    setup_sta(settings);
+    
+    //Re-enable wifi
+    start_and_connect();
 
-    while(1){
-
-        //Re-enable wifi
-        start_and_connect();
-
-        uint16_t length = 0;
-        if(print_get("http://httpbin.org/get", response, &length)) {
-            printf("%.*s", length, response);
-            memset(response, 0xFF, 1000);
-        } else {
-            printf("Error doing GET request");
-        }
-
-        //Disable wifi before going into sleep
-        disable_wifi();
-
-        //Set sleep timer and go into sleep
-        esp_sleep_enable_timer_wakeup(10 * 1000000);
-        ESP_ERROR_CHECK(esp_light_sleep_start());
+    uint16_t length = 0;
+    if(print_get("https://httpbin.org/get", response, &length)) {
+        printf("%.*s\n", length, response);
+        memset(response, 0xFF, 1000);
+    } else {
+        printf("Error doing GET request");
     }
+
+    //Disable wifi before going into sleep
+    disable_wifi();
     free(response);
+}
+
+
+void handle_sensor_wakeup() {
+    //Retrieve data from memory.
+    //Show data...
+}
+
+void handle_reset_wakeup() {
+    //Check if key is held down for 5 sec
+    //Yes: reset settings No: return
+}
+
+void deep_sleep() {
+    //Set sleep timer and go into sleep
+    esp_sleep_enable_timer_wakeup(30 * 1000000);
+    uint64_t pin_mask = (uint64_t)1 << BUTTON_LEFT | (uint64_t)1 << BUTTON_RIGHT;
+    esp_sleep_enable_ext1_wakeup(pin_mask, 0);
+    esp_deep_sleep_start();
 }
 
 void app_main(void)
@@ -46,15 +58,42 @@ void app_main(void)
     setup_flash();
     setup_settings();
     setup_buttons();
-    setup_wifi();
 
     settings_t settings;
     bool settings_found = retrieve_settings(&settings);
 
     if(settings_found) {
-        //Start the gather data task if WiFi settings are set
-        xTaskCreate(gather_data_task, "gather data task", 2048, &settings, 5, NULL);
+        //Depending on how the device was started, run certain tasks
+        esp_sleep_source_t wakeup_cause = esp_sleep_get_wakeup_cause();
+
+        if(wakeup_cause == ESP_SLEEP_WAKEUP_EXT1) {
+            //Check pin num
+            uint64_t pin_mask = esp_sleep_get_ext1_wakeup_status();
+
+            bool did_sensor_wakeup = (pin_mask >> BUTTON_LEFT) == 1;
+            bool did_button_wakeup = (pin_mask >> BUTTON_RIGHT) == 1;
+
+            if(did_button_wakeup) {
+                handle_reset_wakeup();
+            } 
+            else if(did_sensor_wakeup) {
+                handle_sensor_wakeup();
+            }
+        }
+        else if (wakeup_cause == ESP_SLEEP_WAKEUP_TIMER) {
+            setup_wifi();
+            handle_timer_wakeup(&settings);
+        }
+        else {
+            printf("Unknown wakeup cause / Inital startup\n");
+            setup_wifi();
+            handle_timer_wakeup(&settings);
+        }
+
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+        deep_sleep();
     } else {
+        setup_wifi();
         setup_access_point();
     }
     while(1) {}
