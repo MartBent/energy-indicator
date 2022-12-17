c89bfd499d933501e5b3dc9fa86bfea2758b5802
MartBent <martbent99@gmail.com>
2022-12-17 19:29:40 +0100

    Cleanup button.c, removed interrupt usage.

--- main/hardware/buttons.c
+++ main/hardware/buttons.c
@@ -12,27 +12,13 @@ void button_left_handler(void* arg)
     while(1){
         vTaskSuspend(button_left_task_handle);
         vTaskDelay(300 / portTICK_PERIOD_MS);
-        printf("Left\n");
-        uint32_t counter = 0;
-        while(gpio_get_level(BUTTON_LEFT) == 0) {
-
-            vTaskDelay(50 / portTICK_RATE_MS);
-            ++counter;
-
-            if(counter > 100) {
-                erase_settings();
-                esp_restart();
-                break;
-            }
-        }
     }
 }
 void button_right_handler(void* arg)
 {
     while(1){
         vTaskSuspend(button_right_task_handle);
-        printf("Right\n");
-        vTaskDelay(300 / portTICK_PERIOD_MS);
+        vTaskDelay(10000 / portTICK_PERIOD_MS);
     }
 }
 
@@ -41,23 +27,23 @@ void setup_buttons() {
     gpio_pad_select_gpio(BUTTON_LEFT);
     gpio_set_direction(BUTTON_LEFT, GPIO_MODE_INPUT);
     gpio_set_pull_mode(BUTTON_LEFT, GPIO_PULLUP_ONLY);
-    gpio_set_intr_type(BUTTON_LEFT, GPIO_INTR_NEGEDGE);
-    gpio_intr_enable(BUTTON_LEFT);
+    //gpio_set_intr_type(BUTTON_LEFT, GPIO_INTR_NEGEDGE);
+    //gpio_intr_enable(BUTTON_LEFT);
 
     // Initialize the buttons
     gpio_pad_select_gpio(BUTTON_RIGHT);
     gpio_set_direction(BUTTON_RIGHT, GPIO_MODE_INPUT);
     gpio_set_pull_mode(BUTTON_RIGHT, GPIO_PULLUP_ONLY);
-    gpio_set_intr_type(BUTTON_RIGHT, GPIO_INTR_NEGEDGE);
-    gpio_intr_enable(BUTTON_RIGHT);
+    //gpio_set_intr_type(BUTTON_RIGHT, GPIO_INTR_NEGEDGE);
+    //gpio_intr_enable(BUTTON_RIGHT);
 
-    gpio_install_isr_service(0);
+    //gpio_install_isr_service(0);
 
-    gpio_isr_handler_add(BUTTON_LEFT, button_isr, &button_left_task_handle);
-    gpio_isr_handler_add(BUTTON_RIGHT, button_isr, &button_right_task_handle);
+    //gpio_isr_handler_add(BUTTON_LEFT, button_isr, &button_left_task_handle);
+    //gpio_isr_handler_add(BUTTON_RIGHT, button_isr, &button_right_task_handle);
 
-    xTaskCreate(button_left_handler, "button left isr", 2048, NULL, 5, &button_left_task_handle);
-    xTaskCreate(button_right_handler, "button right isr", 2048, NULL, 5, &button_right_task_handle);
+    //xTaskCreate(button_left_handler, "button left isr", 2048, NULL, 5, &button_left_task_handle);
+    //xTaskCreate(button_right_handler, "button right isr", 2048, NULL, 5, &button_right_task_handle);
 }
 
 #endif
